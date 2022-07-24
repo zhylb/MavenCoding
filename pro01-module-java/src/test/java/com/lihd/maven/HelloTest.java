83ac9b99a14a6886841e85cbdac900c887843b5c
zhylb <1851053336@qq.com>
2022-07-24 18:49:22 +0800

    hotfix commit 1

--- pro01-module-java/src/test/java/com/lihd/maven/HelloTest.java
+++ pro01-module-java/src/test/java/com/lihd/maven/HelloTest.java
@@ -3,6 +3,7 @@ package com.lihd.maven;
 import org.junit.Test;
 
 /**
+ * 现在老子在 hot-fix下
  * @author ：葬花吟留别1851053336@qq.com
  * @description：TODO
  * @date ：2022/7/22 10:59
@@ -10,11 +11,16 @@ import org.junit.Test;
 public class HelloTest {
     @Test
     public void testSayHello() {
+        int a = 10;
         Hello hello = new Hello();
         hello.sayHello();
         hello.sayHello();
         hello.sayHello();
         hello.sayHello();
         hello.sayHello();
+        if (a > 10) {
+            System.out.println("a > 10");
+        }
+
     }
 }
